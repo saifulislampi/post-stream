06b0549521fc46f29c99acd67a7a6ac0f806fb17
showkot <shossain@nd.edu>
2025-06-29 00:25:43 -0400

    env file added

---
+++ src/environments.js
@@ -0,0 +1,5 @@
+module.exports = {
+  APPLICATION_ID: "3gMH4Kq9ALoTgvRaYN0STLZPBRBrw6HzlIqytZzf",
+  JAVASCRIPT_KEY: "V1OcAlaeBrZwruPBmMjT2d0nIZ1r5rSI7ONSSPKN",
+  SERVER_URL: "https://parseapi.back4app.com/"
+};
